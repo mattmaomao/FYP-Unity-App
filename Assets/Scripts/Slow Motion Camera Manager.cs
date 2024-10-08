2bc4e0298ec863af12c09b5ab315b541c8ba558d
mattmaomao <matthewcp220@gmail.com>
2024-10-09 01:44:03 +0800

    adjust camera size

--- Assets/Scripts/Slow Motion Camera Manager.cs
+++ Assets/Scripts/Slow Motion Camera Manager.cs
@@ -19,8 +19,9 @@ public class SlowMotionCameraManager : MonoBehaviour
 
     void Start()
     {
-        delayDisplay.rectTransform.sizeDelta = new Vector2((float)Screen.height/webCamTexture.height*webCamTexture.width, Screen.height);
-        delayDisplay.rectTransform.localScale = new Vector2(-1, 1);
+        delayDisplay.rectTransform.sizeDelta = new Vector2(webCamTexture.width, webCamTexture.height);
+        float scale = (float)Screen.height / webCamTexture.height;
+        delayDisplay.rectTransform.localScale = new Vector2(-scale, scale);
 
         timer = 0;
         updateInterval = 1f / FPS;
