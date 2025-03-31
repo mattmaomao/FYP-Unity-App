ae2a85d608393fb4d81f4b452a3a6e57fbe1da38
mattmaomao <matthewcp220@gmail.com>
2025-03-31 12:18:48 +0800

    fixed slow motion cam

--- Assets/Scripts/Slow Motion Camera/Slow Motion Camera Manager.cs
+++ Assets/Scripts/Slow Motion Camera/Slow Motion Camera Manager.cs
@@ -19,9 +19,8 @@ public class SlowMotionCameraManager : MonoBehaviour
     public Button button;
 
     [SerializeField] float timer = 0f;
-    [SerializeField] static float FPS;
-    [SerializeField] static float slowMotionRate = 0.5f;
-    [SerializeField] float frameInterval;
+    [SerializeField] float slowMotionRate = 0.5f;
+    int totalFrameCount = 0;
     [SerializeField] float timeInterval = 4f;
     [SerializeField] float waiter = 0;
     [SerializeField] bool recording = false;
@@ -47,8 +46,6 @@ public class SlowMotionCameraManager : MonoBehaviour
 
         timer = 0;
         recording = false;
-        FPS = 1.0f / Time.deltaTime;
-        frameInterval = 1f / FPS;
         resetLoadingTime();
         remainSlider.gameObject.SetActive(false);
 
@@ -65,59 +62,66 @@ public class SlowMotionCameraManager : MonoBehaviour
             resizeDisplay();
         }
 
-        if ((wait == true) && (readyTime > 0))
-        {
-            wait = true;
-            readyTime -= Time.deltaTime;
-            int seconds = ((int)readyTime);
-            load.gameObject.SetActive(true);
-            load.SetText(seconds.ToString());
-            remainSlider.gameObject.SetActive(false);
-            delayDisplay.texture = null;
+        if (wait) {
+            if (readyTime > 0)
+            {
+                readyTime -= Time.deltaTime;
+                int seconds = ((int)readyTime);
+                load.gameObject.SetActive(true);
+                load.SetText(seconds.ToString());
+                remainSlider.gameObject.SetActive(false);
+                delayDisplay.texture = null;
 
-        }
-        else
-        {
-            wait = false;
-            load.gameObject.SetActive(false);
-            resetLoadingTime();
-            recording = true;
-        }
-
-        if (recording &&(timer < timeInterval))
-        {
-            load.gameObject.SetActive(false);
-            remainSlider.gameObject.SetActive(true);
-            //delayDisplay.texture = null;
-            Texture2D frame = new Texture2D(webCamTexture.width, webCamTexture.height);
-            frame.SetPixels(webCamTexture.GetPixels());
-            frame.Apply();
-            capturedFrames.Add(frame);
-            delayDisplay.texture = frame;
-            timer += frameInterval;
-
-        }
-        else { 
-            recording = false;
-            remainSlider.gameObject.SetActive(false);
+            }
+            else
+            {
+                wait = false;
+                load.gameObject.SetActive(false);
+                resetLoadingTime();
+                recording = true;
+            }
         }
 
-        if ((capturedFrames.Count > 1) && (!recording) && (waiter >= (1f / (FPS * slowMotionRate))))
-        {
-            delayDisplay.texture = capturedFrames[1];
-            capturedFrames[0].hideFlags = HideFlags.HideAndDontSave;
-            Destroy(capturedFrames[0]);
-            capturedFrames.RemoveAt(0);
-            waiter = 0;
-            Debug.Log(capturedFrames.Count);
+        if (recording) {
+            if (timer < timeInterval)
+            {
+                load.gameObject.SetActive(false);
+                remainSlider.gameObject.SetActive(true);
+                //delayDisplay.texture = null;
+                Texture2D frame = new Texture2D(webCamTexture.width, webCamTexture.height);
+                frame.SetPixels(webCamTexture.GetPixels());
+                frame.Apply();
+                capturedFrames.Add(frame);
+                delayDisplay.texture = frame;
+                timer += Time.deltaTime;
+
+            }
+            else { 
+                recording = false;
+                remainSlider.gameObject.SetActive(false);
+                totalFrameCount = capturedFrames.Count;
+            }
         }
-        else if ((capturedFrames.Count == 1) && !recording) {
-            timer = 0;
-            capturedFrames.Clear();
-            wait = true;
-        }
-        else if (waiter < (1f / (FPS * slowMotionRate))){
-            waiter += frameInterval;
+
+        if (!wait && !recording) {
+            float secToPlay = timeInterval / slowMotionRate / totalFrameCount;
+            if ((capturedFrames.Count > 1) && (waiter >= secToPlay))
+            {
+                delayDisplay.texture = capturedFrames[1];
+                capturedFrames[0].hideFlags = HideFlags.HideAndDontSave;
+                Destroy(capturedFrames[0]);
+                capturedFrames.RemoveAt(0);
+                waiter = 0;
+                Debug.Log(capturedFrames.Count);
+            }
+            else if ((capturedFrames.Count == 1) && !recording) {
+                timer = 0;
+                capturedFrames.Clear();
+                wait = true;
+            }
+            else if (waiter < secToPlay){
+                waiter += Time.deltaTime;
+            }
         }
     }
 
@@ -136,6 +140,8 @@ public class SlowMotionCameraManager : MonoBehaviour
 
         timeIntervalSlider.value = 1;
         rateSlider.value = 0.5f;
+        timeInterval = 1f;
+        slowMotionRate = 0.5f;
         prompt.gameObject.SetActive(true);
         timeIntervalSlider.gameObject.SetActive(true);
         rateSlider.gameObject.SetActive(true); 
